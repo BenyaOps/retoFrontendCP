4a7e0864e5e268abe7661c348e125e7aa9635db9
Benjamin Ponce <benjaminponce@Mac-mini-de-Benjamin.local>
2026-02-20 22:40:24 -0500

    update: viite confiig

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: '/',
+  base: '/cp-reto-frontend/',
   resolve: {
     alias: {
       // 2. Definir el alias para que coincida con tu tsconfig
